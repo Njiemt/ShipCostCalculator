e3485a7627caedaeca52ab2ce5f9d96e7ca1a34c
Njiemt <Njiemt@mail.uc.edu>
2026-02-11 11:34:10 -0500

    b

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,13 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
+public class ShipCostCalculator {
+    public static void main() {
+        double itemPrice;
+        double shipCost;
+        Scanner input = new Scanner(System.in);
+        IO.println("How much Does it Cost?");
+        itemPrice = input.nextDouble();
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        if (itemPrice >= 100) {
+            IO.println("Shipping is free for orders over $100");
+        } else {
+            shipCost = itemPrice * 1.02;
+            IO.println("Your Total Cost After Shipping Is " + shipCost);
+        }
+
+    }
 }
