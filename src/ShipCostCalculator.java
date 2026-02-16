c1b99dfc49ddb2c28da730a5952c551a753994fd
Njiemt <Njiemt@mail.uc.edu>
2026-02-16 17:10:39 -0500

    3rd Commit

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -3,6 +3,7 @@ public class ShipCostCalculator {
     public static void main() {
         double itemPrice;
         double shipCost;
+        double finalCost;
         Scanner input = new Scanner(System.in);
         IO.println("How much Does it Cost?");
         itemPrice = input.nextDouble();
@@ -10,8 +11,10 @@ public class ShipCostCalculator {
         if (itemPrice >= 100) {
             IO.println("Shipping is free for orders over $100");
         } else {
-            shipCost = itemPrice * 1.02;
-            IO.println("Your Total Cost After Shipping Is " + shipCost);
+            finalCost = itemPrice * 1.02;
+            shipCost = itemPrice * 0.02;
+            IO.println("your Shipping cost is " + shipCost);
+            IO.println("Your Total Cost After Shipping Is " + finalCost);
         }
 
     }
